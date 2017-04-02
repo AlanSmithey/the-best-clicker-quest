1047517e14239784d2dff01b3f8fef0a48862e27
Aleksei Strukov <striker1982@list.ru>
2017-04-02 20:20:07 +0300

    scripts and structure improvement

--- src/js/main.js
+++ src/js/main.js
@@ -10,59 +10,77 @@
             'https://fonts.googleapis.com/css?family=Griffy|Indie+Flower'
         ],
         images: document.getElementsByTagName('img'),
-        medias: document.querySelectorAll('[data-src]'),
+        iframes: document.getElementsByTagName('iframe'),
+        videos: document.getElementsByTagName('video'),
+        audios: document.getElementsByTagName('audio'),
 
-        getLength: function() {
-            return this.styles.length + this.images.length + this.medias.length;
+        getTotalLength: function() {
+            return this.styles.length + this.images.length + this.iframes.length + this.videos.length + this.audios.length;
         }
     };
 
     function count() {
         counter++;
-        if (counter === elements.getLength()) {
+        if (counter === elements.getTotalLength()) {
             setInterval(function(){
-                document.documentElement.classList.add('loaded'); // optional
-                document.body.classList.remove('pause');
+                document.documentElement.classList.remove('html-loading');
+                document.body.classList.remove('body-pause');
             }, 500);
-            
         }
     }
 
     function stylesLoader() {
-
         for (var i = 0, len = elements.styles.length; i < len; i++) {
             var style = document.createElement('link');
 
             style.href = elements.styles[i];
             style.rel = 'stylesheet';
-            style.onload = count;
+            style.addEventListener('load', count);
 
             document.head.appendChild(style);
         }
     }
 
     function imagesLoader() {
-
         for (var i = 0, len = elements.images.length; i < len; i++) {
 
-            elements.images[i].onload = count;
+            elements.images[i].addEventListener('load', count);
             elements.images[i].removeAttribute('srcset');
         }
     }
+    
+    function iframesLoader() {
+        for (var i = 0, len = elements.iframes.length; i < len; i++) {
+            var src = elements.iframes[i].getAttribute('data-src');
 
-    function mediasLoader() {
-
-        for (var i = 0, len = elements.medias.length; i < len; i++) {
-            var mediaAttr = elements.medias[i].getAttribute('data-src');
-
-            elements.medias[i].onload = count;
-            elements.medias[i].setAttribute('src', mediaAttr);
+            elements.iframes[i].addEventListener('load', count);
+            elements.iframes[i].setAttribute('src', src);
         }
-    };
+    }
+
+    function videosLoader() {
+        for (var i = 0, len = elements.videos.length; i < len; i++) {
+            var src = elements.videos[i].getAttribute('data-src');
+
+            elements.videos[i].addEventListener('canplay', count);
+            elements.videos[i].setAttribute('src', src);
+        }
+    }
+
+    function audiosLoader() {
+        for (var i = 0, len = elements.audios.length; i < len; i++) {
+            var src = elements.audios[i].getAttribute('data-src');
+
+            elements.audios[i].setAttribute('src', src);
+            count();
+        }
+    }
 
     stylesLoader();
     imagesLoader();
-    mediasLoader();
+    iframesLoader();
+    videosLoader();
+    audiosLoader();
 })();
 
 /*-----------------------*
